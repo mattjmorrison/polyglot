21298581b9f6ad6005b3a2701316935e0e3a3b83
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-15 13:39:09 -0500

    cleaned up haskell fibonacci

--- languages/haskell/fibonacci.hs
+++ languages/haskell/fibonacci.hs
@@ -1,14 +1,8 @@
 import System.Environment
-main :: IO ()
 
 main = do x <- getArgs
-          print(fib(num (x !! 0)))
+          print (fib (read (x !! 0)))
 
-num :: String -> Integer
-num = read
-
-
-fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib n
+    | n < 2 = n
+    | otherwise = fib (n-1) + fib (n-2)
