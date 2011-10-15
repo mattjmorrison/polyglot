37f72f234577984a7dba66a83d00c4f2e045c5e1
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-15 01:02:51 -0500

    added fibonacci for Java

---
+++ languages/java/Fibonacci.java
@@ -0,0 +1,13 @@
+class Fibonacci {
+	
+	public static void main(String[] args){
+		System.out.println(fib(Integer.parseInt(args[0])));
+	}
+	
+	public static int fib(int n){
+		if (n < 2)
+			return n;
+		return fib(n - 1) + fib(n - 2);
+	}
+	
+}
--- run/run_java.sh
+++ run/run_java.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+echo "== Java Hi =="
 echo "Compile Java"
 javac ../languages/java/Hi.java
 echo "Execute Java"
 java -cp ../languages/java Hi 
 rm -rf ../languages/java/*.class
+
+echo "== Java Fibonacci =="
+echo "Compile Java"
+javac ../languages/java/Fibonacci.java
+echo "Execute Java"
+java -cp ../languages/java Fibonacci 20
+rm -rf ../languages/java/*.class
+
