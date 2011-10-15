881ab1336a09f6f16d5f6030afe3974be9af1759
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-15 00:56:16 -0500

    updated run_all to be in alphabetical order

--- run/run_all.sh
+++ run/run_all.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-echo == Python ==
-./run_python.sh
-echo == Ruby ==
-./run_ruby.sh
-echo == Perl ==
-./run_perl.sh
-echo == PHP ==
-./run_php.sh
-echo == Node.js ==
-./run_node.sh
-echo == CoffeeScript ==
-./run_coffee.sh
-echo == Clojure ==
-./run_clojure.sh
-echo == XSLT ==
-./run_xslt.sh
-echo == Erlang ==
-./run_erlang.sh
-echo == Java ==
-./run_java.sh
-echo == Scala ==
-./run_scala.sh
-echo == Haskell ==
-./run_haskell.sh
+
 echo == C ==
 ./run_c.sh
+
+echo == Clojure ==
+./run_clojure.sh
+
+echo == CoffeeScript ==
+./run_coffee.sh
+
 echo == C++ ==
 ./run_cpp.sh
 
+echo == Erlang ==
+./run_erlang.sh
+
+echo == Haskell ==
+./run_haskell.sh
+
+echo == Java ==
+./run_java.sh
+
+echo == Node.js ==
+./run_node.sh
+
+echo == Perl ==
+./run_perl.sh
+
+echo == PHP ==
+./run_php.sh
+
+echo == Python ==
+./run_python.sh
+
+echo == Ruby ==
+./run_ruby.sh
+
+echo == Scala ==
+./run_scala.sh
+
+echo == XSLT ==
+./run_xslt.sh
