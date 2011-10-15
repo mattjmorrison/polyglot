c4519ce98f29bd7f69f2c1a129b614892ea7afa1
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-15 11:44:37 -0500

    added fibonacci for xslt

--- run/run_xslt.sh
+++ run/run_xslt.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "== XSLT Hi =="
 xsltproc ../languages/xslt/hi.xslt ../languages/xslt/data/hi.xml
+
+echo "== XSLT Fibonacci =="
+xsltproc ../languages/xslt/fibonacci.xslt ../languages/xslt/data/fibonacci.xml
+
+
