d6deab3965ca324be39ec249892a850f7d550491
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-14 22:32:39 -0500

    added fibonacci for coffeescript

---
+++ languages/coffee/fibonacci.coffee
@@ -0,0 +1,5 @@
+fib = (n) ->
+  return n if n < 2
+  return (fib n-1) + (fib n-2)
+
+console.log fib process.argv[2]
--- run/run_coffee.sh
+++ run/run_coffee.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "== CoffeeScript Hi =="
 coffee ../languages/coffee/hi.coffee
+
+echo "== CoffeeScript Fibonacci =="
+coffee ../languages/coffee/fibonacci.coffee 20
