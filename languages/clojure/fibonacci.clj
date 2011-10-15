9a24ec1a34b992a67bdad52b7ea283b402e0b6c1
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-14 22:25:00 -0500

    added fibonacci for clojure

---
+++ languages/clojure/fibonacci.clj
@@ -0,0 +1,8 @@
+(defn fib [n]
+  (if (< n 2) 
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))
+  )
+)
+
+(println (fib (Integer/parseInt (first *command-line-args*))))
--- run/run_clojure.sh
+++ run/run_clojure.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-java -jar clojure/clojure-1.3.0.jar ../languages/clojure/hi.clj
+CLOJURE_DIR=clojure
+CLOJURE_JAR=$CLOJURE_DIR/clojure-1.3.0.jar
+CLASSPATH=.:$CLOJURE_JAR:$CONTRIB_JAR
+CLOJURE_COMMAND="java -cp .:$CLOJURE_JAR clojure.main"
+
+echo "== Clojure Hi =="
+$CLOJURE_COMMAND ../languages/clojure/hi.clj
+rm -rf *.class
+
+echo "== Clojure Fibonacci 20 =="
+$CLOJURE_COMMAND ../languages/clojure/fibonacci.clj 20
 rm -rf *.class
