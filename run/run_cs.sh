dd7bdf9e9129579b33e23d2f6c71223f002e8c19
Matthew Morrison <matthewmorrison@matthew-morrisons-macbook.local>
2011-10-15 14:02:21 -0500

    added fibonacci for C#

--- run/run_cs.sh
+++ run/run_cs.sh
@@ -5,3 +5,10 @@ gmcs ../languages/cs/hi.cs
 echo "Execute C#"
 mono ../languages/cs/hi.exe
 rm -rf ../languages/cs/hi.exe
+
+echo "== C# Fibonacci =="
+echo "Compile C#"
+gmcs ../languages/cs/fibonacci.cs
+echo "Execute C#"
+mono ../languages/cs/fibonacci.exe 20
+rm -rf ../languages/cs/fibonacci.exe
